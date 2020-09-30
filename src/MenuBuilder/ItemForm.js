d4211d87e31c639fd7a24ad86271266daf311e07
Svetlozar Mihailov <svetliomihailov@gmail.com>
2020-09-30 21:03:35 +0300

    Update ItemForm UI

--- src/MenuBuilder/ItemForm.js
+++ src/MenuBuilder/ItemForm.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import { Copy, Delete } from './icons';
-import { Button, Toolbar, Input, Stack } from '@raketa-cms/raketa-mir';
+import {
+  Button,
+  Toolbar,
+  FormGroup,
+  Label,
+  Input,
+  Stack,
+} from '@raketa-cms/raketa-mir';
 
 export default ({ item, onChange, onRemove, onDuplicate }) => (
   <div>
     <Stack>
-      <Input
-        name='label'
-        label='Label'
-        type='text'
-        wrapperProps={{ style: { marginRight: '0.5em' } }}
-        value={item.label}
-        onChange={(e) =>
-          onChange({
-            id: item.id,
-            label: e.target.value,
-            url: item.url,
-          })
-        }
-      />
-      <Input
-        name='url'
-        label='URL'
-        type='text'
-        value={item.url}
-        onChange={(e) =>
-          onChange({
-            id: item.id,
-            label: item.label,
-            url: e.target.value,
-          })
-        }
-      />
+      <FormGroup>
+        <Label htmlFor='label'>Label</Label>
+
+        <Input
+          name='label'
+          type='text'
+          wrapperProps={{ style: { marginRight: '0.5em' } }}
+          value={item.label}
+          onChange={(e) =>
+            onChange({
+              id: item.id,
+              label: e.target.value,
+              url: item.url,
+            })
+          }
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Label htmlFor='url'>URL</Label>
+
+        <Input
+          name='url'
+          type='text'
+          value={item.url}
+          onChange={(e) =>
+            onChange({
+              id: item.id,
+              label: item.label,
+              url: e.target.value,
+            })
+          }
+        />
+      </FormGroup>
     </Stack>
 
     <Toolbar>
